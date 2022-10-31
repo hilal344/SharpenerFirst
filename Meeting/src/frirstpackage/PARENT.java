de4c96af862fd8e8a93f2b4997a5c6c75da66917
ABDULLAH <abdullahhilal344@gmail.com>
2022-10-31 11:58:43 +0530

    first commit
    pushing all files

---
+++ Meeting/src/frirstpackage/PARENT.java
@@ -0,0 +1,16 @@
+package frirstpackage;
+
+public class PARENT{
+		public static void abc() {
+			System.out.println("this is parent class");
+		}
+		public static void main(String args[]) {
+			PARENT obj = new PARENT();
+			obj.abc();
+		}
+	    }
+
+
+
+
+
